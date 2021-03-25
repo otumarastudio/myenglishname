fbce5bad689cf3d875031601e7cdb0b6b1740cce
otumarastudio <heeungs@gmail.com>
2021-03-25 21:42:10 +0900

    ui update

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Button, Progress} from 'semantic-ui-react'
+import { Icon } from 'semantic-ui-react'
+import { Segment, Input } from 'semantic-ui-react'
 
 function App() {
   return (
 
     <div>
-<p> 안녕하세요</p>
-<Button primary> 알아보기 </Button>
+<h1>Hello World</h1>
+<Icon loading name='spinner' />
+<Icon loading name='certificate' /><Icon loading name='asterisk' />
+
+<Segment inverted>
+  <Input inverted placeholder='Search...' />
+</Segment>
+
 <Progress percent = {55} indicating />
+<Button primary> 분석 해보기 </Button>
+
+
 
     </div>
 
